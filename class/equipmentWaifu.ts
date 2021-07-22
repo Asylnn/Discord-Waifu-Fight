99e67243292047b221775018ed963268bcba4660
Glou-Glou <87784112+Glou-Glou@users.noreply.github.com>
2021-07-22 15:49:40 +0200

    Update equipmentWaifu.ts

--- class/equipmentWaifu.ts
+++ class/equipmentWaifu.ts
@@ -1,13 +1,30 @@
-/* Mother-class*/ 
-export default abstract class equipmentWaifu {
-    protected id: number;
-    protected name: string;
-    protected level: number;
-    protected rarity: string;
-    protected tabModificators: Array<modificator>;
+import modificator from "./types/modificator";
+ 
+export default class equipmentWaifu {
+    private id: number;
+    private name: string;
+    private level: number;
+    private rarity: string;
+    private type: string; //Helmet,chestplate,boots ...
+    private set: string;//Maybe add a class for the set of equipment for different checks
+    private img: string; //URL of the image
+    private tabModificators: Array<modificator>;//The list of modificators
+
+    constructor(id: number,name: string,level: number = 0,rarity: string,type: string,set: string
+    ,img: string,tabModificators: Array<modificator>){
+        this.id = id;
+        this.name = name;
+        this.level = level;
+        this.rarity = rarity;
+        this.type = type;
+        this.set = set;
+        this.img = img;
+        this.tabModificators = tabModificators;
+    }
 
     toString(){
         "id: " + this.id + " name: " + this.name + " level: " + this.level + " rarity: " + 
-        this.rarity + " tabModificators: " + this.tabModificators;
+        this.rarity + " type: " + this.type + " set: " + this.set + " img : " + this.img +
+        " tabModificators: " + this.tabModificators;
     }
 }
