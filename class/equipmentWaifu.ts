a40ee624f15cb8899852d86e4017e4dde4b96e2a
Glou-Glou <87784112+Glou-Glou@users.noreply.github.com>
2021-07-22 15:04:41 +0200

    Update equipmentWaifu.ts

--- class/equipmentWaifu.ts
+++ class/equipmentWaifu.ts
@@ -1,1 +1,13 @@
 /* Mother-class*/ 
+export default abstract class equipmentWaifu {
+    protected id: number;
+    protected name: string;
+    protected level: number;
+    protected rarity: string;
+    protected tabModificators: Array<modificator>;
+
+    toString(){
+        "id: " + this.id + " name: " + this.name + " level: " + this.level + " rarity: " + 
+        this.rarity + " tabModificators: " + this.tabModificators;
+    }
+}
