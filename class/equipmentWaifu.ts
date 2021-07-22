785c70c21fa64bdbcc2ca42ea24b9134357a62f2
Lucas Ait Oulahyane <aitou.lucas@gmail.com>
2021-07-22 20:18:41 +0200

    added some commentary in equipmentWaifu.ts

--- class/equipmentWaifu.ts
+++ class/equipmentWaifu.ts
@@ -1,5 +1,28 @@
-import modificator from "./types/modificator";
- 
+/*
+Ce mini-jeu à l'air pour le moment élitiste en terme de ranking osu!, il faut donc faire en sorte que
+les plus débutants puisse quand meme y participer un minimum =
+- Accompagnement du user par une waifu / l'équipe active de waifus.
+- Refaire le système d'items (Item en classe abstraite).
+
+
+
+
+
+
+
+
+Addition d'une stat :
+Nouvelle stats : chance de double claims, Stat Rare?
+Potentiellement stats disponibles uniquement sur un type d'équipement (épée : XP, bottes : EX etc...)
+
+
+
+- Mini event : VS entre la personne et un de nous (Asyln & QuentinFTW)
+*/
+
+
+import {modificator} from "./types/modificator";
+
 export default class equipmentWaifu {
     private id: string;
     private name: string;
@@ -11,7 +34,7 @@ export default class equipmentWaifu {
     private tabModificators: Array<modificator>;//The list of modificators
 
     //Generating a new instance
-    constructor(id: number,name: string,rarity: string,type: string,set: string
+    constructor(id: string,name: string,rarity: number,type: string,set: string
     ,img: string){
         this.id = id;
         this.name = name;
@@ -20,15 +43,21 @@ export default class equipmentWaifu {
         this.type = type;
         this.set = set;
         this.img = img;
-        this.tabModificators = generateEffect(rarity); /*Generating the number of modificators
-        depending on the rarity*/
+        this.tabModificators = this.generateModificators(rarity); /* Generating the number of modificators
+        depending on the rarity */
+    }
+    upgradeModificator(){} // Upgrade an existing effect when the max of stats
+    generateModificator(){} // Generate a new effect when reaching a milestone
+    generateModificators(rarity){
+      let modificator = []
+    } // Generate the modificator(s) depending on the rarity
+
+    addLevel(){
+
     }
 
-    generateEffect(){}//Generate a new effect when reaching a milestone
-    generateEffect(rarity){} //Generate the modificator(s) depending on the rarity
-
     toString(){
-        "id: " + this.id + " name: " + this.name + " level: " + this.level + " rarity: " + 
+        "id: " + this.id + " name: " + this.name + " level: " + this.level + " rarity: " +
         this.rarity + " type: " + this.type + " set: " + this.set + " img : " + this.img +
         " tabModificators: " + this.tabModificators;
     }
